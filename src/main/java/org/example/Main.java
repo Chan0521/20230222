293e7efcc0f6b89769b9983ffebffbe06d30f8fc
Chan0521 <aflpq369@gmail.com>
2023-02-22 09:50:21 +0900

    프로젝트 세팅

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!\n");
+        System.out.println("-----------\n");
+
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+
+
+    }
+}
